f2c3003619cc53f6640b35b7bd58566188422cce
Ivan Calandra <12525997+ivan-paleo@users.noreply.github.com>
2023-03-14 10:44:34 +0100

    Minor improvements: - Comment commands - Maintenance info dependent on instrument - Report includes time in filename

--- reporting-templates/app.R
+++ reporting-templates/app.R
@@ -1,5 +1,5 @@
-#
-#
+# Shiny app to enter the settings of acquisitions at the IMPALA
+# Download the settings in an editable (XLSX) report
 
 # Load libraries
 library(shiny)
@@ -10,91 +10,153 @@ library(writexl)
 ui <- fluidPage(
 
   # Application title
-  titlePanel("Create a report for your microscope images at the IMaging Platform At LeizA (IMPALA)"),
+  titlePanel("Create a report for your microscope images acquired at the IMaging Platform At LeizA (IMPALA)"),
 
-  # Sidebar with a selection box to select the instrument for which the report will be created
+  # Sidebar
   sidebarLayout(
     sidebarPanel(
+
+      # Input text for user name
       textInput("name", "Your name", "Ivan Calandra"),
+
+      # Selection box to select the instrument for which the report will be created
       selectInput("instrument", "Choose the instrument",
                   c("Axio Imager.Z2 Vario + LSM 800 MAT", "Smartzoom 5")),
+
+      # TraCEr logo - will need to be replaced with IMPALA logo
       img(src = "TraCEr_Logo_black.png", height = 150)
     ),
 
-    # Show tabs
+    # Main panel
     mainPanel(
+
+      # Tabs
       tabsetPanel(
+
+        # Tabs, their UIs will be rendered in the server call below
         tabPanel("General", uiOutput("general")),
         tabPanel("Acquisition", uiOutput("acq")),
         tabPanel("Pre-processing", uiOutput("proc")),
         tabPanel("Abbreviations", tableOutput("abbr")),
+
+        # Tab "Export report"
         tabPanel("Export report", fluidRow(
+
+          # Shows the result of the user input in tables
           tableOutput("general_set"),
           tableOutput("acq_set"),
           tableOutput("proc_set"),
-          downloadButton("downloadReport", "Download Report"))
+
+          # Add a button to download the report
+          downloadButton("downloadReport", "Download Report to XLSX"))
         )
       )
     )
   )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
+
+  # Render tab 'general'
   output$general <- renderUI({
+
+    # create objects with different values depending on the instrument
     if (input$instrument == "Smartzoom 5") {
       soft <- c("Smartzoom", "ZEN core")
       acq_modes <- c("2D", "EDF", "3D", "Stitching")
+
+      # Must be assigned to global environment so that it can be used outside of renderUI()
+      # and inside 'report_general'
       assign("setup", "Steel table on solid concrete base", envir = .GlobalEnv)
+      assign("maintenance", data.frame(Category = "Maintenance", Setting = "", Value = "NA"),
+             envir = .GlobalEnv)
     }
     if (input$instrument == "Axio Imager.Z2 Vario + LSM 800 MAT") {
       soft <- c("ZEN blue", "ZEN core")
       acq_modes <- c("3D Topography", "EDF", "Stitching")
       assign("setup", "Passive anti-vibration table on solid concrete base", envir = .GlobalEnv)
+      assign("maintenance", data.frame(Category = rep("Maintenance", 3),
+                                       Setting = c("Last yearly inspection and calibration by manufacturer",
+                                                   "Last topography correction for used objectives",
+                                                   "Last control for used objectives with the roughness standard (nominal Ra = 0.40 ± 0.05 µm)"),
+                                       Value = c("2022-10-11", "2022-10-11", "2021-12-22")),
+             envir = .GlobalEnv
+      )
     }
+
+    # Create a list of inputs
     tagList(
+
+      # Selection box to select the software used, possible values come from 'soft'
       selectInput("software", "Software", soft),
+
+      # Input text for software version
       textInput("version", "Software version", "2.6 HF 12"),
+
+      # Check box whether Shuttle-and-Find module was used
       checkboxInput("sf", "Shuttle-and-Find module used", FALSE),
+
+      # Check boxes to select which acquisition mode(s) was (were) used
+      # Possible values come from 'acq_modes'
       checkboxGroupInput("acq_mode", "Acquisition modes", acq_modes)
     )
   })
+
+  # Create output for general settings
+  # 'reactive()' is necessary to use input values from above and to export it
   report_general <- reactive({
-    data.frame(
+
+    # Create data.frame() with information to include in the report
+    # Some information is user input, other is pre-defined
+    temp <- data.frame(
       Category = c("User", "Microscope", "Microscope", "Location", "Location", "Location",
-                   "Software", "Acquisition modes", "Maintenance", "Maintenance", "Maintenance"),
+                   "Software", "Acquisition modes"),
       Setting = c("Name", "Manufacturer", "Model", "Facility", "Floor", "Setup",
-                  "Software, version and modules", "",
-                  "Last yearly inspection and calibration by manufacturer",
-                  "Last topography correction for used objectives",
-                  "Last control for used objectives with the roughness standard (nominal Ra = 0.40 ± 0.05 µm)"),
+                  "Software, version and modules", ""),
       Value = c(input$name, "Carl Zeiss Microscopy GmbH", input$instrument,
                 "IMPALA, MONREPOS, Germany",
                 "-1 (basement)", setup,
                 paste0(input$software, " ", input$version, ", Shuttle-and-Find: ",  input$sf),
-                paste(input$acq_mode, collapse = ", "),
-                "2022-10-11", "2022-10-11", "2021-12-22")
+                paste(input$acq_mode, collapse = ", "))
     )
+    rbind(temp, maintenance)
   })
+
+  # Render output for general settings in the tab "Export report" in the table 'general_set'
   output$general_set <- renderTable({
     report_general()
   })
 
+  # Create output for abbreviations
+  # 'reactive()' is necessary to export it
   report_abbr <- reactive({
+
+    # Create data.frame() with abbreviations to include in the report, pre-defined
     data.frame(Abbreviation = c("AU", "HF", "LSM", "NA", "WD", "WF"),
                Explanation = c("Airy unit", "Hot fix", "Laser-scanning confocal microscopy",
                                "Numerical aperture", "Working distance", "Wide-field")
     )
   })
+
+  # Render output for abbreviations in the tab "Abbreviations" in the table 'abbr'
   output$abbr <- renderTable({
     report_abbr()
   })
 
+  # Define what happens when one clicks on the download button
   output$downloadReport <- downloadHandler(
+
+    # Create file name for file to be downloaded
     filename = function() {
-      paste("report_", gsub(" ", "-", input$name), "_", Sys.Date(), ".xlsx", sep = "")
+      paste("report-IMPALA_", gsub(" ", "-", input$name),
+            format(Sys.time(), "_%Y-%m-%d_%H-%M-%S"), ".xlsx", sep = "")
     },
+
+    # Define content
     content = function(file){
+
+      # Write to XLSX, each table in a sheet
       writexl::write_xlsx(list(General_settings = report_general(), Abbreviations = report_abbr()),
                           file)
     }
