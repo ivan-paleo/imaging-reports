dd639f7cde72a22156a5c39f89c7ccef25ef3a85
Ivan Calandra <12525997+ivan-paleo@users.noreply.github.com>
2023-03-20 16:40:33 +0100

    New tab objectives

--- reporting-templates/app.R
+++ reporting-templates/app.R
@@ -1,19 +1,24 @@
 # Shiny app to enter the settings of acquisitions at the IMPALA
 # Download the settings in an editable (XLSX) report
 
-# Load libraries
+#####
+
+# 1. Load libraries
 library(shiny)
 library(writexl)
 #library(shinyforms)
 
-# Define UI
+#####
+
+# 2. Define UI
 ui <- fluidPage(
 
-  # Application title
+  # 2.1. Application title
   titlePanel("Create a report for your microscope images acquired at the IMaging Platform At LeizA (IMPALA)"),
 
-  # Sidebar
   sidebarLayout(
+
+    # 2.2. Sidebar
     sidebarPanel(
 
       # Input text for user name
@@ -27,7 +32,7 @@ ui <- fluidPage(
       img(src = "TraCEr_Logo_black.png", height = 150)
     ),
 
-    # Main panel
+    # 2.3. Main panel
     mainPanel(
 
       # Tabs
@@ -35,6 +40,7 @@ ui <- fluidPage(
 
         # Tabs, their UIs will be rendered in the server call below
         tabPanel("General", uiOutput("general")),
+        tabPanel("Objectives", uiOutput("obj")),
         tabPanel("Acquisition", uiOutput("acq")),
         tabPanel("Pre-processing", uiOutput("proc")),
         tabPanel("Abbreviations", tableOutput("abbr")),
@@ -45,6 +51,8 @@ ui <- fluidPage(
           # Shows the result of the user input in tables
           h2("General settings"),
           tableOutput("general_set"),
+          h2("Objectives"),
+          tableOutput("obj_set"),
           h2("Acquisition settings"),
           tableOutput("acq_set"),
           h2("Pre-processing settings"),
@@ -58,10 +66,13 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic
+#####
+
+# 3. Define server logic
 server <- function(input, output) {
 
-  # Render tab 'general'
+  # 3.1. Tab General
+  # 3.1.1. Render tab 'general'
   output$general <- renderUI({
 
     # create objects with different values depending on the instrument
@@ -106,7 +117,7 @@ server <- function(input, output) {
     )
   })
 
-  # Create output for general settings
+  # 3.1.2 Create output for general settings
   # 'reactive()' is necessary to use input values from above and to export it
   report_general <- reactive({
 
@@ -126,62 +137,59 @@ server <- function(input, output) {
     rbind(temp, maintenance)
   })
 
-  # Render output for general settings in the tab "Export report" in the table 'general_set'
+  # 3.1.3. Render output for general settings in the tab "Export report" in the table 'general_set'
   output$general_set <- renderTable({
     report_general()
   })
 
 
-  # Render tab 'Acquisition'
-  output$acq <- renderUI({
+  # 3.2. Tab Objectives
+  # 3.2.1. Render tab 'obj'
+  output$obj <- renderUI({
 
     # create objects with different values depending on the instrument
     if (input$instrument == "Smartzoom 5") {
-      objectives <- paste0("PlanApoD ", c(1.6, 5), "x / NA = ", c(0.1, 0.3), " / WD = ", c(36, 30), " mm")
-      obj_use <- "Color image"
-
-      # Must be assigned to global environment so that it can be used outside of renderUI()
-      # and inside 'report_acq'
-      #assign("setup", "Steel table on solid concrete base", envir = .GlobalEnv)
+      assign("objectives", paste0("PlanApoD ", c(1.6, 5), "x / NA = ", c(0.1, 0.3), " / WD = ", c(36, 30), " mm"), envir = .GlobalEnv)
+      obj_use <- c("Color image", "Not used")
     }
-
     if (input$instrument == "Axio Imager.Z2 Vario + LSM 800 MAT") {
-      objectives <- paste0("C Epiplan-Apochromat ", c(5, 10, 20, 20, 50, 50, 50), "x / NA = ",
+      assign("objectives", paste0("C Epiplan-Apochromat ", c(5, 10, 20, 20, 50, 50, 50), "x / NA = ",
                            format(c(0.20, 0.40, 0.22, 0.70, 0.55, 0.75, 0.95), digits = 2), " / WD = ",
-                           c(21, 5.4, 12, 1.3, 9, 1, 0.22)," mm")
-      obj_use <- c("Preview scan", "Coordinate system", "Color image", "3D topography")
+                           c(21, 5.4, 12, 1.3, 9, 1, 0.22)," mm"), envir = .GlobalEnv)
+      obj_use <- c("Preview scan", "Coordinate system", "Color image", "3D topography", "Not used")
     }
 
     # Create a list of inputs
     tagList(
-
-      # Selection box to select the software used, possible values come from 'soft'
-      #selectInput("software", "Software", soft),
-
-      # Input text for software version
-      #textInput("version", "Software version", "2.6 HF 12"),
-
-      # Check box whether Shuttle-and-Find module was used
-      #checkboxInput("sf", "Shuttle-and-Find module used", FALSE),
-
-      # Check boxes to select which objective(s) was (were) used
-      # Possible values come from 'objectives'
-      # Does not work yet - ideally a table of checkboxes
-      fluidRow(
-        #for (i in objectives) {
-          column(4, paste(objectives, sep = "\n", collapse = "\n")),
-          column(2, checkboxGroupInput("preview", "Preview scan", rep(" ", length(objectives)))),
-          column(2, checkboxGroupInput("coord", "Coordinate system", rep(" ", length(objectives)))),
-          column(2, checkboxGroupInput("colo", "Color image", rep(" ", length(objectives)))),
-          column(2, checkboxGroupInput("topo", "3D topography", rep(" ", length(objectives))))
-        #}
-        #checkboxGroupInput("objs", "Objectives", objectives, width = "100%")
-      )
+      h2("Specify how you used each objective"),
+      h4("Make sure to select 'Not used' for the objectives you have not used"),
+      lapply(seq_along(objectives), function(i) {
+        selectInput(paste0("obj", i), objectives[i], choices = obj_use, width = "100%", multiple = TRUE)
+      })
     )
   })
 
+  # 3.2.2. Create output for objective settings
+  # 'reactive()' is necessary to use input values from above and to export it
+  report_obj <- reactive({
 
-  # Create output for abbreviations
+    # Create data.frame() with information to include in the report
+    # Some information is user input, other is pre-defined
+    temp <- data.frame(
+      Objective = objectives,
+      Use = sapply(seq_along(objectives), function(i) paste(input[[paste0('obj', i)]], collapse = ", "))
+    )
+    temp <- temp[temp$Use != "Not used", ]
+  })
+
+  # 3.2.3. Render output for general settings in the tab "Export report" in the table 'general_set'
+  output$obj_set <- renderTable({
+    report_obj()
+  })
+
+
+  # 3.5. Tab Abbreviations
+  # 3.5.1. Create output for abbreviations
   # 'reactive()' is necessary to export it
   report_abbr <- reactive({
 
@@ -192,29 +200,34 @@ server <- function(input, output) {
     )
   })
 
-  # Render output for abbreviations in the tab "Abbreviations" in the table 'abbr'
+  # 3.5.2. Render output for abbreviations in the tab "Abbreviations" in the table 'abbr'
   output$abbr <- renderTable({
     report_abbr()
   })
 
-  # Define what happens when one clicks on the download button
+
+  # 3.6. Define what happens when one clicks on the download button
   output$downloadReport <- downloadHandler(
 
-    # Create file name for file to be downloaded
+    # 3.6.1. Create file name for file to be downloaded
     filename = function() {
-      paste("report-IMPALA_", gsub(" ", "-", input$name),
+      paste("IMPALA-report_", gsub(" ", "-", input$name),
             format(Sys.time(), "_%Y-%m-%d_%H-%M-%S"), ".xlsx", sep = "")
     },
 
-    # Define content
+    # 3.6.2. Define content
     content = function(file){
 
       # Write to XLSX, each table in a sheet
-      writexl::write_xlsx(list(General_settings = report_general(), Abbreviations = report_abbr()),
-                          file)
+      writexl::write_xlsx(list(General_settings = report_general(), Objectives = report_obj(), Abbreviations = report_abbr()), file)
     }
   )
 }
 
-# Run the application
+#####
+
+# 4. Run the application
 shinyApp(ui = ui, server = server)
+
+
+# END OF CODE #
